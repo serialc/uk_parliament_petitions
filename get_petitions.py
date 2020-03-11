e0ca294972aea271a92f9eab10b2e6d6f364f302
serialc <cyrille.mdc@gmail.com>
2020-03-11 11:33:21 +0100

    Added full path for cron

--- get_petitions.py
+++ get_petitions.py
@@ -3,16 +3,19 @@ from datetime import datetime
 
 # retrieve data
 req = requests.get("https://petition.parliament.uk/petitions.csv?state=open")
+data_path = "/home/pi/Documents/uk_parliament_petitions/"
 
 if req.ok:
     lines = req.text.strip().split('\n')
+
+    print("Found ", len(lines), " lines.")
 
     if len(lines) > 1:
         # datetime object containing current date and time
         now = datetime.now()
 
         # save it
-        fh = open('data/' + now.strftime("%Y-%m-%d_%H-%M-%S") + '_ukpp.csv', 'w')
+        fh = open(data_path + 'data/' + now.strftime("%Y-%m-%d_%H-%M-%S") + '_ukpp.csv', 'w')
 
         fh.write(req.text)
 
